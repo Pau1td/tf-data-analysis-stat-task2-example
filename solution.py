2a55353f83b69f3ccf4715d34f4efaa20d80103b
Pau1td <110543968+Pau1td@users.noreply.github.com>
2023-04-06 14:14:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,5 +14,5 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     loc = x.mean()
     scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return 2*loc - 2*scale * norm.ppf(1 - alpha / 2) + a, \
-           2*loc + 2*scale * norm.ppf(alpha / 2) + a
+    return 2*loc + 2*scale * norm.ppf(1 - alpha) + a, \
+           2*loc + 2*scale * norm.ppf(p) + a
